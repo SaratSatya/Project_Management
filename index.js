39317721db92becb95cd6559e794fa559cb14fc1
Sarat Satya <surapaneneisaratsatya@gmail.com>
2025-09-13 07:57:40 +0530

    Added .env in the project

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Start of backend Project");
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./.env",
+})
+
+let myusername=process.env.usersrname;
+let mydatabase=process.env.database;
+console.log("Username is: "+myusername);
+console.log("Database is: "+mydatabase);
+
+console.log("start of awesome backend Project");
